ee08f1e220bfe5964517e63f2476d69a0d7e68c0
Jan Ernsting <jan.ernsting@gmail.com>
2016-11-30 18:09:26 +0100

    Move conversion into dedicated object

---
+++ src/NumberToRomanNumeralConverter.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+
+public class NumberToRomanNumeralConverter {
+  
+  private Map<Integer, Integer> subtractivePairs = new HashMap<>();
+  private List<Integer> numberCategories = new ArrayList<>();
+  private Map<Integer, String> numberToNumeral = new HashMap<>();
+
+  public NumberToRomanNumeralConverter() {
+    setupLookup();
+    setupDescendingNumberCategories(numberToNumeral.keySet());
+    setupSubtractivePairs();
+  }
+
+  public String arabicToRoman(Integer number) {
+    StringBuilder result = new StringBuilder();
+    for (Integer numberCategory : numberCategories) {
+      if (subtractivePairs.containsKey(numberCategory)) {
+        Integer subtractor = subtractivePairs.get(numberCategory);
+        if (number == numberCategory - subtractor) {
+          result.append(numberToNumeral.get(subtractor));
+          number += subtractor;
+        }
+      }
+      while (number >= numberCategory) {
+        result.append(numberToNumeral.get(numberCategory));
+        number -= numberCategory;
+      }
+    }
+    return result.toString();
+  }
+
+  private void setupLookup() {
+    numberToNumeral.put(1, "I");
+    numberToNumeral.put(5, "V");
+    numberToNumeral.put(10, "X");
+    numberToNumeral.put(50, "L");
+  }
+
+  private void setupDescendingNumberCategories(Set<Integer> numberSet) {
+    numberCategories.addAll(numberSet);
+    Collections.sort(numberCategories);
+    Collections.reverse(numberCategories);
+  }
+
+  private void setupSubtractivePairs() {
+    subtractivePairs.put(5, 1);
+    subtractivePairs.put(10, 1);
+    subtractivePairs.put(50, 10);
+  }
+
+}
--- src/RomanNumeralsKata.java
+++ src/RomanNumeralsKata.java
@@ -1,10 +1,3 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import org.junit.Test;
 
 import static org.hamcrest.CoreMatchers.is;
@@ -13,68 +6,24 @@ import static org.junit.Assert.*;
 
 public class RomanNumeralsKata {
 
+  NumberToRomanNumeralConverter converter = new NumberToRomanNumeralConverter();
+  
   @Test
   public void shouldConvertSimpleNumbers() {
-    assertThat(arabicToRoman(1), is("I"));
-    assertThat(arabicToRoman(5), is("V"));
-    assertThat(arabicToRoman(10), is("X"));
+    assertThat(converter.arabicToRoman(1), is("I"));
+    assertThat(converter.arabicToRoman(5), is("V"));
+    assertThat(converter.arabicToRoman(10), is("X"));
   }
 
   @Test
   public void shouldAddNumerals() {
-    assertThat(arabicToRoman(23), is("XXIII"));
+    assertThat(converter.arabicToRoman(23), is("XXIII"));
   }
   
   @Test
   public void shouldHandleSubtractiveRomanNumerals() {
-    assertThat(arabicToRoman(4), is("IV"));
-    assertThat(arabicToRoman(9), is("IX"));
-    assertThat(arabicToRoman(40), is("XL"));
-  }
-
-  private String arabicToRoman(Integer number) {
-    Map<Integer, String> numberToNumeral = setupLookup();
-    List<Integer> numberCategories = setupDescendingNumberCategories(numberToNumeral.keySet());
-    Map<Integer, Integer> subtractivePairs = subtractivePairs();
-    
-    StringBuilder result = new StringBuilder();
-    for (Integer numberCategory : numberCategories) {
-      if (subtractivePairs.containsKey(numberCategory)) {
-        Integer subtractor = subtractivePairs.get(numberCategory);
-        if (number == numberCategory - subtractor) {
-          result.append(numberToNumeral.get(subtractor));
-          number += subtractor;
-        }
-      }
-      while (number >= numberCategory) {
-        result.append(numberToNumeral.get(numberCategory));
-        number -= numberCategory;
-      }
-    }
-    return result.toString();
-  }
-
-  private Map<Integer, String> setupLookup() {
-    Map<Integer, String> numberToNumeral = new HashMap<>();
-    numberToNumeral.put(1, "I");
-    numberToNumeral.put(5, "V");
-    numberToNumeral.put(10, "X");
-    numberToNumeral.put(50, "L");
-    return numberToNumeral;
-  }
-
-  private List<Integer> setupDescendingNumberCategories(Set<Integer> numberSet) {
-    List<Integer> numberCategories = new ArrayList<>(numberSet);
-    Collections.sort(numberCategories);
-    Collections.reverse(numberCategories);
-    return numberCategories;
-  }
-  
-  private Map<Integer, Integer> subtractivePairs() {
-    HashMap<Integer, Integer> pairs = new HashMap<>();
-    pairs.put(5, 1);
-    pairs.put(10, 1);
-    pairs.put(50, 10);
-    return pairs;
+    assertThat(converter.arabicToRoman(4), is("IV"));
+    assertThat(converter.arabicToRoman(9), is("IX"));
+    assertThat(converter.arabicToRoman(40), is("XL"));
   }
 }
